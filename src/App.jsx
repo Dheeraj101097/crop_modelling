86aee3cc64d19a86957393a499b6de357210201e
Dheeraj101097 <devishree1005@gmail.com>
2025-06-25 03:53:19 +0530

    analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,22 @@ import ResultsSection from "./components/ResultsSection";
 import StudyAreaSection from "./components/StudyAreaSection";
 import ContactSection from "./components/ContactSection";
 import ScrollProgress from "./components/ScrollProgress";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-green-50 to-yellow-50">
-      <ScrollProgress />
-      <Hero />
-      <WorkflowSection />
-      <FeaturesSection />
-      <ResultsSection />
-      <StudyAreaSection />
-      {/* <ContactSection /> */}
-    </div>
+    <>
+      <div className="min-h-screen bg-gradient-to-b from-green-50 to-yellow-50">
+        <ScrollProgress />
+        <Hero />
+        <WorkflowSection />
+        <FeaturesSection />
+        <ResultsSection />
+        <StudyAreaSection />
+        {/* <ContactSection /> */}
+      </div>
+      <Analytics />
+    </>
   );
 }
 
